72f150d5581fd1b6cdbea7dbeb039d7eb516b59c
Bachi-code <241422@student.pwr.edu.pl>
2020-04-18 14:51:09 +0200

    time

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 from pynput import keyboard #pip3 install pynput
 import time
 
-def callb(key): 		#What to do on key-release
-	ti1 = str(time.time() - t )[0:5] #converting float to str, slicing the float
-	print('The key',key,'is pressed for',ti1,'seconds')
-	return False 		#stop detecting more key-releases
-def callb1(key):		#What to do on key-press
-	return False		#stop detecting more key-presses
+while True:
 
-with keyboard.Listener(on_press = callb1) as listener1:	#setting code for listening key-press
-	listener1.join()
+	def callb(key): 		#What to do on key-release
+		ti1 = str(time.time() - t )[0:5] #converting float to str, slicing the float
+		print('The key',key,'is pressed for',ti1,'seconds')
+		return False 		#stop detecting more key-releases
+	def callb1(key):		#What to do on key-press
+		return False		#stop detecting more key-presses
 
-t = time.time()
+	with keyboard.Listener(on_press = callb1) as listener1:	#setting code for listening key-press
+		listener1.join()
 
-with keyboard.Listener(on_release = callb) as listener:	#setting code for listening key-release
-	listener.join()
+	t = time.time()
+
+
+	with keyboard.Listener(on_release = callb) as listener:	#setting code for listening key-release
+		listener.join()
