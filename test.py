1b9cfb63f61a45f1a477556fcd1658a8dc69c0e9
Bachi-code <241422@student.pwr.edu.pl>
2020-04-19 13:52:06 +0200

    Dodanie flight time

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from pynput import keyboard #pip3 install pynput
 import time
 
+ti2 = 0
+
 while True:
 
 	def callb(key): 		#What to do on key-release
+		global ti2
 		ti1 = str(time.time() - t )[0:5] #converting float to str, slicing the float
 		print('The key',key,'is pressed for',ti1,'seconds')
+		ti2 = time.time()
 		return False 		#stop detecting more key-releases
 	def callb1(key):		#What to do on key-press
 		return False		#stop detecting more key-presses
@@ -14,7 +18,8 @@ while True:
 		listener1.join()
 
 	t = time.time()
-
+	czas2 = str(t - ti2)[0:6] # flight time latency - czas miedzy puszczeniem 1 klawisza a wcisnieciem nastepnego
+	print("Flight time:", czas2)
 
 	with keyboard.Listener(on_release = callb) as listener:	#setting code for listening key-release
 		listener.join()
