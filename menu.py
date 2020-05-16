483ecc20b6adbe13fa72a9eec5f0e29e17127f39
Ksz-cb <241475@student.pwr.edu.pl>
2020-05-16 16:24:46 +0200

    Menu file change

--- menu.py
+++ menu.py
@@ -1,12 +1,15 @@
-import test.py
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+clear_screen()
 print("Witamy w projekcie biometrycznym na temat keystrokingu")
 print("All rights reserved for Krzystof Miodonski and Adam Bachera")
 print("-----------------------------------------------------------")
 print("Twoje opcje:")
 print("1. Stworz nowy profil. (Pamietaj, jest to dlugi proces.)")
 
-wybor=input("\nPowiedz nam co chcesz zrobic:")
+wybor = int(input("\nPowiedz nam co chcesz zrobic: "))
 
 if wybor == 1:
-    execfile('python test.py')
+    exec(open("./test.py").read())
+
