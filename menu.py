eeab2c7f3f13cabbec2ff6a84f0708c9951e623b
Ksz-cb <241475@student.pwr.edu.pl>
2020-05-16 16:28:43 +0200

    Contetx menu

--- menu.py
+++ menu.py
@@ -4,9 +4,11 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 clear_screen()
-print("Witamy w projekcie biometrycznym na temat keystrokingu")
-print("All rights reserved for Krzystof Miodonski and Adam Bachera")
-print("-----------------------------------------------------------")
+print("|--------------------------------------------------------------|")
+print("|   Witamy w projekcie biometrycznym na temat keystrokingu     |")
+print("|                                                              |")
+print("| All rights reserved for Krzystof Miodonski and Adam Bachera  |")
+print("|--------------------------------------------------------------|\n")
 print("Twoje opcje:")
 print("1. Stworz nowy profil. (Pamietaj, jest to dlugi proces.)")
 
