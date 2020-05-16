29f96cc07a234b915f2c96979c74e7167a5ea8d5
Ksz-cb <241475@student.pwr.edu.pl>
2020-05-16 16:18:08 +0200

    Menu file change

--- menu.py
+++ menu.py
@@ -1,1 +1,12 @@
-for x
+import test.py
+
+print("Witamy w projekcie biometrycznym na temat keystrokingu")
+print("All rights reserved for Krzystof Miodonski and Adam Bachera")
+print("-----------------------------------------------------------")
+print("Twoje opcje:")
+print("1. Stworz nowy profil. (Pamietaj, jest to dlugi proces.)")
+
+wybor=input("\nPowiedz nam co chcesz zrobic:")
+
+if wybor == 1:
+    execfile('python test.py')
