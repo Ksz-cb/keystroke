6156eefbca0f9ec13cc5b367507b2b917e40eff4
Bachi-code <241422@student.pwr.edu.pl>
2020-06-01 13:33:36 +0200

    hasla

--- test2.py
+++ test2.py
@@ -9,7 +9,7 @@ def callb(key):
     t = time.time()
 
     if key != Key.enter:
-        keyPressData.append("{0}-{1}".format(key,t))
+        keyPressData.append("{0}-{1}".format(str(key),t))
 
 
 def callb1(key):
@@ -45,50 +45,73 @@ def backspaceKeys(keyPreRe):
 
 
 def licz(digraph):
-    directory = "./adam/"
-    data_file = open(directory + digraph, 'r+')
-    data1 = []
-    for line in data_file:
-        data1.extend([float(i) for i in line.split()])
+    directory = "./" + user + "/"
+    try:
+        data_file = open(directory + digraph, 'r+')
+        data1 = []
+        for line in data_file:
+            data1.extend([float(i) for i in line.split()])
 
-    mu = np.mean(data1)
-    sigma = np.std(data1)
+        mu = np.mean(data1)
+        sigma = np.std(data1)
 
-    return mu, sigma
+        return mu, sigma
+    except FileNotFoundError:
+        mu = 0
+        sigma = 0
+        return mu, sigma
 
 
 def authenticate(digraphtime, digraphletters):
-    score = 50
+    global score
     for i in range(len(digraphtime)):
         mu, sigma = licz("{}.txt".format(digraphletters[i]))
-        if mu - sigma < digraphtime[i] < mu + sigma:
-            score += 2
+        if mu == 0 and sigma == 0:
+            continue
         else:
-            score -= 10
-        print(score)
+            if mu - 2*sigma < digraphtime[i] < mu + 2*sigma:
+                score += 2
+            else:
+                score -= 10
+            print(score)
 
 
+keyPressData = []
+keyReleaseData = []
+run2 = True
 i=1
-score = 50
+# password = "UvJeh*c!5y"
+password = "u"
 meanlist= []
-for textlist in open('./textEXMPL.txt', 'r').readlines():
-    print("Entry", i, "text: ")
-    print(textlist)
-    keyPressData = []
-    keyReleaseData = []
-    with Listener(on_press=callb, on_release=callb1) as listener:
-        x = input()
-        listener.join()
-    i += 1
-    x=''
-    # print(keyPressData)
-    # print(keyReleaseData)
-    # print(digraphtime(keyPressData, keyReleaseData))
-    # print(digraphletters(keyPressData, keyReleaseData))
-    keypress2 = backspaceKeys(keyPressData)
-    keyrelease2 = backspaceKeys(keyReleaseData)
-    digraphLetters = digraphletters(keypress2, keyrelease2)
-    digraphTime = digraphtime(keypress2, keyrelease2)
-    print(digraphLetters)
-    print(digraphTime)
-    authenticate(digraphTime, digraphLetters)
+print("Zaloguj sie")
+user = input("Podaj nazwe profilu: ")
+
+if os.path.isdir('./' + user):
+    while run2:
+        score = 50
+        password2 = input("Podaj hasło do profilu: ")
+        if password == password2:
+            while score > 0:
+                keyPressData = []
+                keyReleaseData = []
+                with Listener(on_press=callb, on_release=callb1) as listener:
+                    listener.join()
+                i += 1
+                # print(keyPressData)
+                # print(keyReleaseData)
+                # print(digraphtime(keyPressData, keyReleaseData))
+                # print(digraphletters(keyPressData, keyReleaseData))
+                keypress2 = backspaceKeys(keyPressData)
+                keyrelease2 = backspaceKeys(keyReleaseData)
+                digraphLetters = digraphletters(keypress2, keyrelease2)
+                digraphTime = digraphtime(keypress2, keyrelease2)
+                # print(digraphLetters)
+                # print(digraphTime)
+                authenticate(digraphTime, digraphLetters)
+                print(score)
+                if score <= 0:
+                    continue
+        else:
+            print("Błędne hasło")
+else:
+    print("Nie ma takiego profilu")
